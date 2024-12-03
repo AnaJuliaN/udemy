621498fe6e37b9f062938781a3633a12d7639379
AnaJuliaN <ana.nunes@aluno.ifsp.edu.br>
2024-12-03 20:13:36 -0300

    iniciando projeto orçamento pessoal

---
+++ ES6/APP ORÇAMENTO PESSOAL/app.js
@@ -0,0 +1,58 @@
+//criando um objeto
+class Despesa {
+  constructor(ano, mes, dia, tipo, descricao, valor) {
+    this.ano = ano
+    this.mes = mes
+    this.dia = dia
+    this.tipo = tipo
+    this.descricao = descricao
+    this.valor = valor
+  }
+}
+
+//salvando o obj no armazenamento local do navegador
+class Bd {
+
+  constructor() {
+    let id = localStorage.getItem('id')
+    if(id === null){
+      localStorage.setItem('id', 0)
+    }
+  }
+  getProximoId() {
+    let proximoId = localStorage.getItem('id') //null
+    return parseInt(proximoId) + 1
+  }
+  gravar(d) {
+    let id = this.getProximoId()
+    //convertendo o obj d em uma string no formato JSON
+    localStorage.setItem(id, JSON.stringify(d))
+
+    localStorage.setItem('id', id)
+  }
+}
+
+let bd = new Bd()
+
+//função chamada quando o usuário clicar no botão
+function cadastrarDespesa() {
+
+  let ano = document.getElementById('ano')
+  let mes = document.getElementById('mes')
+  let dia = document.getElementById('dia')
+  let tipo = document.getElementById('tipo')
+  let descricao = document.getElementById('descricao')
+  let valor = document.getElementById('valor')
+
+  //criando um novo objeto despesa
+  let despesa = new Despesa(
+    ano.value, 
+    mes.value, 
+    dia.value, 
+    tipo.value, 
+    descricao.value, 
+    valor.value
+  )
+
+  bd.gravar(despesa)
+}
